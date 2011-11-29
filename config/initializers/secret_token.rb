e977c3dc2a77e3ff23720d074cd09034baf29e28
Timothy Ludikar <tludikar@Tims-MacBook-Pro-2.local>
2011-11-28 21:58:31 -0500

    Add RoR system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autoassist::Application.config.secret_token = '95d4e4d70d178d753fdf150a021bab1fecfaf7ac2d337411efc65edee731b9302fdf0719242c64e61f5c0cc49d788990675532fde3479ee0bf6c804239712fd7'
